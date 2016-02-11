7c2e1060067bd53c0fd55a21facb4f9648303465
Farasath Ahamed <mefarazath@gmail.com>
2016-02-11 11:12:04 +0530

    multithreading the endpoint

--- src/org/uom/cse/communication/server/webservice/SearchServicePublisher.java
+++ src/org/uom/cse/communication/server/webservice/SearchServicePublisher.java
@@ -2,6 +2,8 @@ package org.uom.cse.communication.server.webservice;
 
 
 import javax.xml.ws.Endpoint;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class SearchServicePublisher {
     public static final String SERVICE_NAME = "searchService";
@@ -11,7 +13,12 @@ public class SearchServicePublisher {
 
     public static void publish(String ipAddress, int port, SearchService service) {
         String urlString = "http://" + ipAddress + ":" + port + "/" + SERVICE_NAME;
-        Endpoint.publish(urlString, service);
+
+        ExecutorService es = Executors.newFixedThreadPool(5);
+        Endpoint ep = Endpoint.create(service);
+        ep.setExecutor(es);
+        ep.publish(urlString);
+      //  Endpoint.publish(urlString, service);
         System.out.println("Search service published at " + urlString);
     }
 
